360a85c4c426074f1ff32148df6b9e7dc78ba447
Ajaypal Ghuman <asingh0328@yahoo.com>
2023-03-30 01:34:16 -0400

    added private method and create action to controller

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,6 +1,19 @@
 class SignupsController < ApplicationController
 
     def new 
-        @signup = Signup.new
+      @signup = Signup.new
+      end
+  
+    def create 
+      @signup = Signup.new(signup_params)
+      if @signup.save 
+      redirect_to '/thanks'
     end
-end
+  end
+  
+    private
+    def signup_params
+    params.require(:signup).permit(:email)
+    end
+  end
+  
